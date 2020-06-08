39305cb83cf406077b0317c382f686fa1276e638
dsopata <dsopata@csod.com>
2020-06-09 01:52:45 +0200

    simplifiers example

--- src/Result.java
+++ src/Result.java
@@ -1,9 +1,22 @@
+import org.lsmp.djep.xjep.XJep;
+import org.nfunk.jep.Node;
+import org.nfunk.jep.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 
 public class Result implements ResultInterface {
+
+    private final boolean useJsLib = false;
+    private final boolean useXjep = true;
+
     @Override
     public String getFormula(String tableName) {
         try {
@@ -11,12 +24,40 @@ public class Result implements ResultInterface {
             field.setAccessible(true);
             String[] bestResultArray = (String[]) field.get(GeneticAlgorithm.class);
             String bestResult = Arrays.toString(bestResultArray);
-            System.out.println("Best result: " + bestResult);
-            System.out.println(1);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+
+            if(useJsLib) {
+                return jsLibSimplify(bestResult);
+            }
+
+            if(useXjep) {
+                return xJepSimplify(bestResult);
+            }
+
+            return bestResult;
+        } catch (NoSuchFieldException | IllegalAccessException | ScriptException | FileNotFoundException | ParseException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+
+
+    private String xJepSimplify(String bestResult) throws ParseException {
+        XJep xjep = new XJep();
+        xjep.setImplicitMul(true);
+        xjep.setAllowUndeclared(true);
+
+        Node node = xjep.parse(bestResult);
+        Node simp = xjep.simplify(node);
+
+        return simp.toString();
+    }
+
+    private String jsLibSimplify(String bestResult) throws ScriptException, FileNotFoundException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(new FileReader("lib/math.js"));
+
+        return engine.eval("math.simplify(\'" + bestResult + "\')").toString();
     }
 }
--- src/nodes/Node.java
+++ src/nodes/Node.java
@@ -64,14 +64,15 @@ public abstract class Node {
 
     String bracket(boolean left) {
         switch (this.level) {
-            case 0:
-                return "";
-            case 1:
-                return left ? "(" : ")";
-            case 2:
-                return left ? "[" : "]";
+//            case 0:
+//                return "";
+//            case 1:
+//                return left ? "(" : ")";
+//            case 2:
+//                return left ? "[" : "]";
             default:
-                return left ? "{" : "}";
+                return left ? "(" : ")";
+
         }
     }
 }
